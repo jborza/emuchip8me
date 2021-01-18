16cb3a3e3573adfa6274c8d70ba88d4c9cef93ae
jborza <jborza@gmail.com>
2021-01-18 18:18:21 +0100

    CPU thread so the ROMs run on their own

--- src/com/jborza/chip8me/Chip8Canvas.java
+++ src/com/jborza/chip8me/Chip8Canvas.java
@@ -13,6 +13,9 @@ public class Chip8Canvas extends Canvas implements CommandListener {
 
     RomStorage romStorage;
 
+    Thread thread;
+    private CPUThread cpuThread;
+
     //CHIP-8 specifics
     CPU cpu;
 
@@ -41,6 +44,8 @@ public class Chip8Canvas extends Canvas implements CommandListener {
 
         romStorage = new RomStorage();
 
+        cpuThread = new CPUThread();
+        thread = new Thread(cpuThread);
         setCommandListener(this);
         //TODO we could also modify commands at runtime
         addCommand(CMD_RESET);
@@ -55,23 +60,29 @@ public class Chip8Canvas extends Canvas implements CommandListener {
     }
 
     private void reset() {
+        cpuThread.stop();
+        thread = null;
+        thread = new Thread(cpuThread);
         cpu = new CPU();
         cpu.loadRom(romStorage.getRom(rom));
         cpu.loadFont();
         cpu.reset();
+        cpuThread.cpu = cpu;
+        thread.start();
+        repaint();
     }
 
     public void keyPressed(int code) {
         lastKeyCode = code;
         //decode keys into CHIP-8 keys
         int chip8KeyCode = Keymap.getChip8Key(code);
-        if(chip8KeyCode != Keymap.KEY_INVALID)
+        if (chip8KeyCode != Keymap.KEY_INVALID)
             cpu.state.keys[chip8KeyCode] = true;
     }
 
-    public void keyReleased(int code){
+    public void keyReleased(int code) {
         int chip8KeyCode = Keymap.getChip8Key(code);
-        if(chip8KeyCode != Keymap.KEY_INVALID)
+        if (chip8KeyCode != Keymap.KEY_INVALID)
             cpu.state.keys[chip8KeyCode] = false;
     }
 
@@ -85,7 +96,7 @@ public class Chip8Canvas extends Canvas implements CommandListener {
         scale15Lookup = new int[]{0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43, 45, 46, 48, 49, 51, 52, 54, 55, 57, 58, 60, 61, 63, 64, 66, 67, 69, 70, 72, 73, 75, 76, 78, 79, 81, 82, 84, 85, 87, 88, 90, 91, 93, 94};
     }
 
-    private void paintDisplayRam(Graphics g){
+    private void paintDisplayRam(Graphics g) {
         if (scale == 1) {
             if (scale15x) {
                 //experimental 1.5x scale
@@ -123,8 +134,8 @@ public class Chip8Canvas extends Canvas implements CommandListener {
 
         g.setColor(0xFFFFFF);
 //        g.fillRect(0, 0, getWidth(), getHeight());
-        if(scale15x)
-            g.fillRect(0,0, 96, 48);
+        if (scale == 1 && scale15x)
+            g.fillRect(0, 0, 96, 48);
         else
             g.fillRect(offsetX, offsetY, Chip8HW.DISPLAY_WIDTH * scale, Chip8HW.DISPLAY_HEIGHT * scale);
 
@@ -133,8 +144,8 @@ public class Chip8Canvas extends Canvas implements CommandListener {
         paintDisplayRam(g);
 
         //3410: 96x55
-        g.setColor(0x0);
-//        g.drawString("k:" + lastKeyCode + " scale:" + scale + " PC:" + CPU.charToHex(cpu.state.PC), 0, getHeight(), Graphics.BOTTOM | Graphics.LEFT);
+        g.setColor(0xFFFFFF);
+//        g.drawString("ticks:" + cpu.state.total_cycles, 0, getHeight(), Graphics.BOTTOM | Graphics.LEFT);
     }
 
     int scale15Lookup[];
@@ -167,5 +178,57 @@ public class Chip8Canvas extends Canvas implements CommandListener {
     public void setRom(String rom) {
         this.rom = rom;
         reset();
+    }
+
+    class CPUThread implements Runnable {
+        public CPUThread() {
+            this.isRunning = true;
+        }
+
+        private boolean isRunning;
+
+        public void stop() {
+            isRunning = false;
+        }
+
+        public CPU cpu;
+
+        public void run() {
+            isRunning = true;
+            System.out.println("CPU Thread started ? " + isRunning);
+            while (isRunning) {
+                cpu.emulate_op();
+                //once per 50 frames
+                boolean draw_stable = false;
+                if(draw_stable) {
+                    if (cpu.state.total_cycles % 50 == 0)
+                        repaint();
+                    if (cpu.state.total_cycles % 10 == 0) {
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+                else{
+                    if(cpu.state.draw_flag){
+                        repaint();
+                    }
+                    if (cpu.state.total_cycles % 16 == 0) {
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+//                if(cpu.state.draw_flag){
+//                    cpu.state.draw_flag = false;
+//                    repaint();
+//                }
+            }
+            System.out.println("CPU Thread stopped");
+            //reset
+            isRunning = true;
+        }
     }
 }
